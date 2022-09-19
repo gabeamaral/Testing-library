325f092e2803bdaf123678f22376e717e80bfd71
Gabriel Duarte <gabrielduarte1405@gmail.com>
2022-09-19 16:50:08 -0300

    Requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('Os botões de filtragem por tipo possuem o nome correto', () => {
+    renderWithRouter(<App />);
+    const botao = screen.getByRole('button', { name: /Fire/i });
+    expect(botao).toBeInTheDocument();
+    userEvent.click(botao);
+  });
+  test('Os botões de filtragem por tipo possuem o'
+  + ' data-testid=pokemon-type-button exceto o botão All', () => {
+    renderWithRouter(<App />);
+    const botao = screen.getAllByTestId('pokemon-type-button');
+    const tipos = 7;
+    expect(botao).toHaveLength(tipos);
+  });
+  test('É possível clicar no botão de filtragem All', () => {
+    renderWithRouter(<App />);
+    const botaoAll = screen.getByRole('button', { name: /All/i });
+    expect(botaoAll).toBeInTheDocument();
+    userEvent.click(botaoAll);
+  });
+});
