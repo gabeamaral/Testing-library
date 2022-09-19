e89e48ba36dc150d67ccf87c4774ac800e9b14cb
Gabriel Duarte <gabrielduarte1405@gmail.com>
2022-09-19 15:32:46 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import Favorite from '../pages/FavoritePokemons';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon'
+  + ' found, caso a pessoa não tenha pokémons favoritos;', () => {
+    renderWithRouter(<Favorite />);
+    const frase = screen.getByText(/No favorite pokemon found/i);
+
+    expect(frase).toBeInTheDocument();
+  });
+});
